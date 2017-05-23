6f7d75109ce84f262a31d69b3d18a435eea83705
linfujian <linfujian1989@gmail.com>
2017-05-23 13:14:30 +0800

    use react in spring

--- src/main/resources/js/app.js
+++ src/main/resources/js/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import Hello from '../components/Hello';
+import Hello from './components/Hello.js';
 
 ReactDOM.render(
 	<Hello />,
 	document.getElementById('app')
 );
+
